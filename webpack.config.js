e3d4abc42b5975314e03a87637dbd53483b90226
Quentin Machard <quentin.machard@gmail.com>
2020-11-13 21:33:20 +0100

    :lipstick: Enhance style

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ Encore
 
   // will require an extra script tag for runtime.js
   // but, you probably want this, unless you're building a single-page app
-  .enableSingleRuntimeChunk()
+  // .enableSingleRuntimeChunk()
 
   /*
    * FEATURE CONFIG
@@ -45,7 +45,7 @@ Encore
   .enableBuildNotifications()
   .enableSourceMaps(!Encore.isProduction())
   // enables hashed filenames (e.g. app.abc123.css)
-  .enableVersioning(Encore.isProduction())
+  // .enableVersioning(Encore.isProduction())
 
   // enables @babel/preset-env polyfills
   // .configureBabelPresetEnv((config) => {
